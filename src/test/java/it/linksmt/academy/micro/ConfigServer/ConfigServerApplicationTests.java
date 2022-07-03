04defea521902dffe90c0dcf47415a8e3da1aec1
CONTEV <vincenzo.conte@linksmt.it>
2022-07-03 16:50:01 +0200

    First Commit

---
+++ src/test/java/it/linksmt/academy/micro/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.linksmt.academy.micro.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
